68d41bdc7b5ec1db5c858a4becd5b3f6425e96b7
anasswalha95 <anas.swalha@yahoo.com>
2021-04-07 04:06:56 +0300

    done

--- challenges/array-binary-search/BinarySearch.java
+++ challenges/array-binary-search/BinarySearch.java
@@ -4,6 +4,61 @@ import java.util.ArrayList;
 
 public class BinarySearch {
 
+   public static int binarySearch (int[] arr, int searchKey){
+        int arrLength=arr.length;
+      //  int lastIndex=arrLength-1;
+        int firstIndex=0;
+        int  middleElementIndex=arrLength/ 2;
+        System.out.println(Arrays.toString(arr));
+        if(arrLength % 2==0){ // check if the array is odd or even
+            System.out.println( "prosition "+middleElementIndex);
+            if(arr[middleElementIndex] == searchKey &&  arrLength == 1){
+                System.out.println(arr[middleElementIndex]+"even block");
+                return middleElementIndex;
+            }else if(arrLength == 1 ){
+                return -1;
+            }
+            else if(searchKey > arr[middleElementIndex]  ){
+//
+                return  binarySearch(Arrays.copyOfRange(arr, middleElementIndex--, arrLength), searchKey)+middleElementIndex;
+            }
+            else if(searchKey < arr[ middleElementIndex]  ){
+              //  System.out.println(Arrays.toString(Arrays.copyOfRange(arr, firstIndex, middleElementIndex)));
+                return  binarySearch(Arrays.copyOfRange(arr, firstIndex, middleElementIndex), searchKey)-middleElementIndex;
+            }
 
 
+
+
+        }
+
+        else {
+            System.out.println( "prosition "+middleElementIndex);
+            if(arr[middleElementIndex] == searchKey  && arrLength == 1){
+                System.out.println(arr[middleElementIndex]+"odd block");
+                return middleElementIndex;
+            }else if(arrLength == 1  ){
+                return -1;
+            }
+            else if(searchKey >arr[middleElementIndex]  ){
+                //System.out.println( "prosition"+middleElementIndex);
+                return binarySearch(Arrays.copyOfRange(arr, middleElementIndex++, arrLength), searchKey)+middleElementIndex;
+            }
+            else if(searchKey < arr[middleElementIndex]  ){
+                return  binarySearch(Arrays.copyOfRange(arr, firstIndex, middleElementIndex), searchKey)-middleElementIndex;
+            }
+
+        }
+        return -1;
+
+
+    }
+
+ public static void main(String[] args) {
+
+
+        int [] arr ={1,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59};
+    
+        System.out.println(binarySearch(arr, 28));
+    }
 }
--- challenges/array-binary-search/testBinarySearch.java
+++ challenges/array-binary-search/testBinarySearch.java
@@ -4,6 +4,21 @@ import java.util.ArrayList;
 
 public class testBinarySearch {
     
+ @Test public void testBinarySearch() {
+        int[] test1=  {4,8,15,16,23,42}; 
+         int[] test2=  {11,22,33,44,55,66,77};
+         int[] test3=  {1, 2, 3, 5, 6, 7};
+         int[] expect1 ={2};
+         int[] expect2 ={-1};
+         int[] expect3 ={-1};
+        
+
+   
+
+        assertArrayEquals("test expect output is "+expect1,expect1,BinarySearch.insertShiftArray(test1,15));
+        assertArrayEquals("test expect output is "+expect2,expect2,BinarySearch.insertShiftArray(test2,90));
+        assertArrayEquals("test expect output is "+expect2,expect3,BinarySearch.insertShiftArray(test3, 4));
+    }
 
 
 }
