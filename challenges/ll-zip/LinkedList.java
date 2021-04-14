418646c536681d19c0ad5b8e1a8a99f4b117dedf
anasswalha95 <anas.swalha@yahoo.com>
2021-04-14 11:30:00 +0300

    code challenge 8

--- challenges/ll-zip/LinkedList.java
+++ challenges/ll-zip/LinkedList.java
@@ -1,6 +1,5 @@
 package fundamentals;
 public class LinkedList<N> {
-
     Node<N> head;
     Node<N> tail;
     int nodesCounter=0;
@@ -13,7 +12,6 @@ public class LinkedList<N> {
 
     public boolean includes(N value) {
         Node<N> current= this.head;
-
         while(current !=null){
             if (current.value.equals(value))
                 return true;
@@ -23,37 +21,26 @@ public class LinkedList<N> {
     }
 
     public void append(N value){
-
-
             Node<N> current = head;
-
-
             if(head==null){
                 insert(value);
-
                 return;
             }
             while (current.next != null ) {
-
                 current = current.next;
             }
-
-             this.nodesCounter++;
-
+            this.nodesCounter++;
             current.next = new Node<N>(value,current.next);
             tail=  current.next;
     }
 
     public void insertBefore(N value, N newVal){
-
-
         Node<N> current = head;
         Node<N>  nNode;
         if(current ==null || current.next == null){
             insert(newVal);
         }else {
             while (true) {
-
             if(current.next == null){
                 nNode =new Node<>(newVal,null);
                 nNode.next=current.next;
@@ -76,15 +63,12 @@ public class LinkedList<N> {
     public void insertAfter(N value,N newVal) {
         Node<N> current = head;
         Node<N> nNode;
-
         if (current == null) {
             insert(newVal);
 
         } else if (current.next == null) {
             append(newVal);
-
         } else {
-
             while (current != null) {
                 if (current.value == value) {
                     nNode= new Node<>(newVal,null);
@@ -96,10 +80,7 @@ public class LinkedList<N> {
                 current = current.next;
             }
         }
-
-
     }
-
 
     public String kthFromEnd(int k){
         Node<N> current = head;
@@ -108,19 +89,14 @@ public class LinkedList<N> {
         if(head==null){ //check if the linked list is empty or not
             return "The list is empty";
         }
-
         if (k > this.nodesCounter )
             return "The provided index is Greater than the list length";
-
         if (k == this.nodesCounter ) //check if the linked list is equal to 1 or only has 1 element inside
             return String.format("The value is %s", current.value);
-
         if (k == 0 )
             return String.format("The value is %s", this.tail.value);
-
         if (k < 0 )
             return String.format("The provided index %s must be positive number", k);
-
 
         while (current != null) {
             if (k == tempNodesCounter-1) {
@@ -129,26 +105,48 @@ public class LinkedList<N> {
             --tempNodesCounter;
             current = current.next;
         }
-
-
-
-
         return String.format("The value is %s", current.value);
+    }
+
+
+    public LinkedList zipLists(LinkedList listOne, LinkedList listTwo ){
+
+        Node<N> currentListOne = listOne.head;
+        Node<N> currentListTwo = listTwo.head;
+        if (listOne.head == null && listTwo.head == null){
+           return null;
+        }
+        if (listOne.head == null && listTwo.head != null){
+            //listOne.head =  listTwo.head;
+            head=listTwo.head;
+            return listTwo ;
+        }
+        if (listTwo.head == null  && listOne.head != null){
+            head=listOne.head;
+            return listOne ;
+        }
+        while ( currentListOne != null ){
+            currentListOne = currentListOne.next;
+        }
+        while ( currentListTwo != null ){
+
+            listOne.append(currentListTwo.value);
+            currentListTwo=currentListTwo.next;
+        }
+//
+//        listOne.append(listTwo.head);
+        head=listOne.head;
+        return listOne;
     }
 
     public String toString(){
         Node current= head;
         String text="";
-
         while(current !=null){
             text += "{ "+ current.value +" }" + " -> " ;
             current= current.next;
         }
-
         text+="Null";
-
         return text;
-
     }
-
 }
--- challenges/ll-zip/README.md
+++ challenges/ll-zip/README.md
@@ -1,29 +1,23 @@
 # Challenge Summary
 
-expand singly Linked list by adding new method to search for values in the linked by index
+To combine two linked lists togather
 
 ## Challenge Description
 
-create a new methoed that searches for a value based on an index start from the right to the left and check for:
-
-1. if the index is greater than the length of the linked list
-2. if the index and the length of the list are the same
-3. if the index provided is negative
-4. if the linked list is of a size 1
-5. if the index is in the middle of the linked list
+create a new methoed called ll-zip that accept 2 linked list and combine them togather
 
 ## Approach & Efficiency
 
-The approach was considered is to validate the test cases if exists first then search for the desired value by the index.
+The approach was considered is to validate the test cases if exists first then append the elements in the second linked list to  the first.
 Teh complixity is O(n).
 
 ## Solution
 
-![linked list 3](https://github.com/anassawalha95/data-structures-and-algorithms/blob/main/Data-Structures/assests/kthFromEnd1.jpg?raw=true)
-![linked list 3](https://github.com/anassawalha95/data-structures-and-algorithms/blob/main/Data-Structures/assests/kthFromEnd2.jpg)
-![linked list 3](https://github.com/anassawalha95/data-structures-and-algorithms/blob/main/Data-Structures/assests/kthFromEnd3.jpg)
+![linked list 3](https://github.com/anassawalha95/data-structures-and-algorithms/blob/main/assests/ll-zip1.jpg)
+![linked list 3](https://github.com/anassawalha95/data-structures-and-algorithms/blob/main/assests/ll-zip2.jpg)
 
-[code](https://github.com/anassawalha95/data-structures-and-algorithms/tree/main/Data-Structures/ll-kth-from-end)
+[code](https://github.com/anassawalha95/data-structures-and-algorithms/tree/main/challenges/ll-zip)
+
 
 - [x] Top-level README “Table of Contents” is updated
 - [x] Feature tasks for this challenge are completed
