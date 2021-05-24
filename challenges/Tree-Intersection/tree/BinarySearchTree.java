87eae851d5a8d72c4b2656bfbd92b983a7c26976
anasswalha95 <anas.swalha@yahoo.com>
2021-05-25 01:29:33 +0300

    tree intersection

---
+++ challenges/Tree-Intersection/tree/BinarySearchTree.java
@@ -0,0 +1,54 @@
+package tree;
+
+public class BinarySearchTree<BTS> {
+    private Node <Integer> root;
+
+    public
+    BinarySearchTree(Node<Integer> root){
+        this.root=root;
+    }
+    public Node <Integer> add(Integer  value){
+
+        this.root=  addNewNode(this.root,value);
+
+        return  this.root;
+    }
+
+    private Node<Integer> addNewNode(Node <Integer> root, Integer  value){
+
+        if (root == null) {
+            return new Node<Integer>(value);
+        }
+        if (value < root.value) {
+            root.left = addNewNode(root.left, value);
+        } else if (value > root.value) {
+            root.right = addNewNode(root.right, value);
+        } else {
+            return root;
+        }
+        return root;
+    }
+
+    public
+    Node getRoot() {
+        return root;
+    }
+
+    public boolean  contains (Integer  value){
+        Node <Integer> current = root;
+        while (current !=null){
+            if(current.value == value){
+                return true;
+            }
+            else if (value < current.value) {
+                current = current.left;
+            }
+            else if (value > current.value) {
+                current = current.right;
+            }
+        }
+
+        return false;
+    }
+
+}
